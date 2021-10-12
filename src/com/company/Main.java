93a78e79733f14eec7e3409c6b0ba804d1b54e13
Timhansen1990 <timx1776@edu.easj.dk>
2021-10-12 11:16:49 +0200

    første redigering

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,28 @@
 package com.company;
 
 public class Main {
+
+    private static String userName1;
+    private static String userName2;
+    private static String userName3;
+    private String Username3;
     public static void main(String[] args) {
 
+        System.out.print("Username: ");
+        userName1 = Users.player1.nextLine();
+        System.out.println("Player 1: " + userName1 );
+
+        System.out.print("Username: ");
+        userName2 = Users.player2.nextLine();
+        System.out.println("Player 2: " + userName2 );
+
+        System.out.print("Username: ");
+        userName3 = Users.player3.nextLine();
+        System.out.println("Player 3: " + userName3 );
+
+        System.out.println("|| Player1 || Player 2 || Player3  ||");
+        System.out.print("||   " + userName1 + "   || ");
+        System.out.print(" " + userName2 + "  || ");
+        System.out.print("" + userName3 + " || ");
     }
 }
--- src/com/company/Users.java
+++ src/com/company/Users.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
 public class Users {
+
+    static Scanner player1 = new Scanner(System.in);
+    static Scanner player2 = new Scanner(System.in);
+    static Scanner player3 = new Scanner(System.in);
+
+    public Scanner getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Scanner player1) {
+        this.player1 = player1;
+    }
+
+    public Scanner getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Scanner player2) {
+        this.player2 = player2;
+    }
+
+    public Scanner getPlayer3() {
+        return player3;
+    }
+
+    public void setPlayer3(Scanner player3) {
+        this.player3 = player3;
+    }
 }
