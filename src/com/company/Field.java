7ed78170e82cf09245e452d65335671c91e64012
Martin Rohde <mart3b24@edu.easj.dk>
2021-10-12 10:05:09 +0200

    Field started

---
+++ src/com/company/Field.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Field {
+    String place;
+    int price;
+
+    public Field(String place, int price) {
+        this.place = place;
+        this.price = price;
+    }
+}
+
